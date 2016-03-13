608a1961f9f5b2d0c359a88ee078e9634fbaf915
Nassif Bourguig <nbourguig@gmail.com>
2016-03-13 21:09:16 +0100

    update kahlan config sample

--- src/.kahlan-config.php
+++ src/.kahlan-config.php
@@ -14,10 +14,3 @@ Filter::register('run.testCase', function ($chain) {
     return $chain->next();
 });
 Filter::apply($this, 'run', 'run.testCase');
-
-
-Filter::register('stop.testCase', function ($chain) {
-    box('spec')->get('testCase')->destroy();
-    return $chain->next();
-});
-Filter::apply($this, 'stop', 'stop.testCase');
